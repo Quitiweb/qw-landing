70c70e46a6d08ee1afc43c0a080201cddf2e8135
Rafael Ruiz <rafael.ruiz.romero@gmail.com>
2025-11-09 11:59:32 +0100

    Added a new way to manage the blog posts to maintain the main page styles.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
@@ -7,8 +6,6 @@ import ProjectsCarousel from '@/components/ProjectsCarousel';
 import Blog from '@/components/Blog';
 import Newsletter from '@/components/Newsletter';
 import Footer from '@/components/Footer';
-import { Toaster } from '@/components/ui/toaster';
-import BlogPost from '@/pages/blog/BlogPost';
 
 function Home() {
   return (
@@ -59,16 +56,4 @@ function Home() {
   );
 }
 
-function App() {
-  return (
-    <div className="min-h-screen overflow-x-hidden">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-      </Routes>
-      <Toaster />
-    </div>
-  );
-}
-
-export default App;
+export default Home;
