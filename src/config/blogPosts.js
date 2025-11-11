2a970c1e9f1533497937019f7394c034f6cc5d68
Rafael <quitiweb@gmail.com>
2025-11-11 11:05:48 +0100

    Create centralized blog posts configuration file

---
+++ src/config/blogPosts.js
@@ -0,0 +1,24 @@
+const blogPosts = [
+  {
+    slug: 'mierdificacion',
+    title: 'Mierdificacion',
+    excerpt: 'An exploration of Mierdificacion and its impact.',
+    date: '2025-11-11',
+    readTime: '5 min',
+    category: 'Technology',
+    image: 'path/to/mierdificacion-image.jpg',
+    component: Mierdificacion,
+  },
+  {
+    slug: 'mestanza',
+    title: 'Mestanza',
+    excerpt: 'Insightful thoughts on Mestanza and its developments.',
+    date: '2025-11-11',
+    readTime: '4 min',
+    category: 'Culture',
+    image: 'path/to/mestanza-image.jpg',
+    component: Mestanza,
+  }
+];
+
+export default blogPosts;
